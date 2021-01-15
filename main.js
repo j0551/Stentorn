0a74a165d669e1b48bf20f8085bb45aee6a5b17e
Carl-Gustaf <ewerbring@gmail.com>
2021-01-15 12:28:17 +0100

    la till funktion på drop

--- main.js
+++ main.js
@@ -1,11 +1,18 @@
-function testing(){
-    console.log("hello")
-};
-
-testing();
-
-
 $(function() {
     $(".sten").draggable();
   });
   
+
+
+  $(function() {
+  $( ".center" ).droppable({
+    tolerance: 'touch',
+    drop: function( event, ui ) {
+console.log("i den har funktionen kan vi sanda coordinater till databas")
+      $(this)
+      .addClass( "color" )
+}
+
+}
+);
+});
