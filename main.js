4de1b4432f5b9389c095758e10e66366ae5f2f83
Carl-Gustaf <ewerbring@gmail.com>
2021-01-26 17:20:55 +0100

    Skicka variabel mellan filer

--- main.js
+++ main.js
@@ -1,18 +1,79 @@
+
+
+ var tableChild =[];
+ let data={};
+ window.onload = function () {
+
+  var children = document.getElementById("stencontainer").children;
+  console.log(children.length)
+
+
+};
+
 $(function() {
     $(".sten").draggable();
   });
   
 
-
   $(function() {
-  $( ".center" ).droppable({
+  $( "body" ).droppable({
+
     tolerance: 'touch',
-    drop: function( event, ui ) {
-console.log("i den har funktionen kan vi sanda coordinater till databas")
-      $(this)
-      .addClass( "color" )
+
+    drop: function() {
+      $('body').mouseup(function(e){
+      var offset = $(this).offset();
+      let x = e.clientX - offset.left;
+      let y = e.clientY - offset.top;
+      $('#x_axis').html(x);
+      $('#y_axis').html(y);
+      data = {
+             "x" : x,
+             "y" : y
+          }
+        });
+        var children = document.getElementById("stencontainer").children;
+
+        for (var i = 0; i < children.length; i++) {
+          tableChild = children[i];
+          console.log(tableChild.offsetLeft, children)
+        }
+
+        // console.log(data);
+
+        
+        $(this)
+        .addClass( "color" )
 }
 
 }
 );
 });
+
+
+
+
+// $(document).ready(function() {
+//   $('body').click(function(e) {
+//    var offset = $(this).offset();
+//    let x = e.clientX - offset.left;
+//    let y = e.clientY - offset.top;
+//    $('#x_axis').html(x);
+//    $('#y_axis').html(y);
+//    let data = {
+//           "x" : x,
+//           "y" : y
+//        }
+//        console.log(e);
+//       //  console.log("i den har funktionen kan vi sanda coordinater till databas"+ data)
+// //         data = JSON.stringify(data);
+// //    $.ajax({
+// //        type: 'POST',
+// //        url: "/reg_click/",
+// //        contentType: 'application/json',
+// //        data: data,
+// //        cache: false,
+// //        success: function (result) { console.log(result); }
+// //  });
+//   });
+//  });
